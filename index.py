025665cd0786977beef4558206c315433cc1af1b
sakis <sakispotbullas@gmail.com>
2019-07-16 12:19:36 +0300

    full handwritten number recognition

---
+++ index.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+
+
+(x_train , y_train),(x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train,axis=1)
+x_test = tf.keras.utils.normalize(x_test,axis=1)
+
+model = tf.keras.models.Sequential()
+
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10,activation=tf.nn.softmax))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train,y_train, epochs=1)
+
+val_loss, val_acc = model.evaluate(x_test, y_test)
+
+print("==============After train================")
+
+print(val_loss,val_acc)
+
+
+
+
+
+
+
+
+
+predictions = model.predict([x_test])
+print(np.argmax(predictions[15]))
+
+
+plt.imshow(x_test[15])
+plt.show()
